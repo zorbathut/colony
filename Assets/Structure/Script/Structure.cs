d6899e2ef6afaa1207b898f638dfd8e141c52670
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-13 08:22:53 -0800

    Set up structure gizmo drawing.

--- Assets/Structure/Script/Structure.cs
+++ Assets/Structure/Script/Structure.cs
@@ -3,5 +3,11 @@ using System.Collections;
 
 public class Structure : MonoBehaviour
 {
+    [SerializeField] int m_Width = 1;
+    [SerializeField] int m_Length = 1;
 
+    public virtual void OnDrawGizmos()
+    {
+        GizmoUtility.DrawSquare(new Vector3(transform.position.x - 1.5f * m_Width, 0f, transform.position.z - 1.5f * m_Length), new Vector3(transform.position.x + 1.5f * m_Width, 0f, transform.position.z + 1.5f * m_Length));
+    }
 }
