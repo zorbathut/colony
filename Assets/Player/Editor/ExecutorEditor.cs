8a697b77069c751e286199ed060d5364cec7e2ef
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-23 01:29:22 -0800

    Add a sane editor for the Executor.

---
+++ Assets/Player/Editor/ExecutorEditor.cs
@@ -0,0 +1,25 @@
+﻿using Rotorz.ReorderableList;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Executor))]
+public class ExecutorEditor : Editor
+{
+    private SerializedProperty m_ScriptsProperty;
+    
+    private void OnEnable()
+    {
+        m_ScriptsProperty = serializedObject.FindProperty("m_Scripts");
+    }
+      
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+
+        ReorderableListGUI.Title("Scripts");
+        ReorderableListGUI.ListField(m_ScriptsProperty);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
