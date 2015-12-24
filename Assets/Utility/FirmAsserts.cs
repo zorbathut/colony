c4d2b7eb670c82e866259ca4b70d9f028c1612f0
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-24 05:07:22 -0800

    Remove a namespace from another project of mine I copied from, whoops

--- Assets/Utility/FirmAsserts.cs
+++ Assets/Utility/FirmAsserts.cs
@@ -5,50 +5,47 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-namespace Trickshot
+public class FirmAsserts : MonoBehaviour
 {
-    public class FirmAsserts : MonoBehaviour
+    #if UNITY_EDITOR
+    HashSet<string> m_Ignored = new HashSet<string>();
+
+    bool m_WasPlaying;
+
+    public void OnEnable()
     {
-        #if UNITY_EDITOR
-        HashSet<string> m_Ignored = new HashSet<string>();
+        Application.logMessageReceived += Logger;
+        EditorApplication.playmodeStateChanged += StateChange;
+    }
 
-        bool m_WasPlaying;
-
-        public void OnEnable()
+    void StateChange()
+    {
+        if (m_WasPlaying && !EditorApplication.isPlaying)
         {
-            Application.logMessageReceived += Logger;
-            EditorApplication.playmodeStateChanged += StateChange;
+            m_Ignored.Clear();
         }
 
-        void StateChange()
-        {
-            if (m_WasPlaying && !EditorApplication.isPlaying)
-            {
-                m_Ignored.Clear();
-            }
+        m_WasPlaying = EditorApplication.isPlaying;
+    }
 
-            m_WasPlaying = EditorApplication.isPlaying;
-        }
-
-        void Logger(string text, string stackTrace, LogType type)
+    void Logger(string text, string stackTrace, LogType type)
+    {
+        if (EditorApplication.isPlaying && type == LogType.Assert)
         {
-            if (EditorApplication.isPlaying && type == LogType.Assert)
+            string signature = stackTrace;
+            if (!m_Ignored.Contains(signature))
             {
-                string signature = stackTrace;
-                if (!m_Ignored.Contains(signature))
+                // No good way to reset these afterwards, so we just clobber the old values - this actually still breaks a bit, unfortunately
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                bool result = EditorUtility.DisplayDialog("Assert", text + "\n" + stackTrace, "Skip", "Skip Forever");
+                if (result == false)
                 {
-                    // No good way to reset these afterwards, so we just clobber the old values - this actually still breaks a bit, unfortunately
-                    Cursor.lockState = CursorLockMode.None;
-                    Cursor.visible = true;
-
-                    bool result = EditorUtility.DisplayDialog("Assert", text + "\n" + stackTrace, "Skip", "Skip Forever");
-                    if (result == false)
-                    {
-                        m_Ignored.Add(signature);
-                    }
+                    m_Ignored.Add(signature);
                 }
             }
         }
-        #endif
     }
+    #endif
 }
