9715ddfea215f5bcdc5ae57fc1c17c3ee82b8eb2
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-26 08:53:41 -0800

    Add debug override for quests.

--- Assets/Player/ScriptQuestEvaluate.cs
+++ Assets/Player/ScriptQuestEvaluate.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class ScriptQuestEvaluate : Script
 {
+    static bool s_debugOverrideNext = false;
+
+    public virtual void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            s_debugOverrideNext = true;
+        }
+    }
+
     public override bool Execute()
     {
+        if (s_debugOverrideNext)
+        {
+            s_debugOverrideNext = false;
+            return true;
+        }
+
         return Manager.instance.EvaluateQuests();
     }
 }
