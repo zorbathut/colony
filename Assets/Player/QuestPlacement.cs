e54ad3cf61dab157a77c6e517233fb994d97b0c2
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-27 04:00:59 -0800

    Add removal flag for QuestPlacement.

--- Assets/Player/QuestPlacement.cs
+++ Assets/Player/QuestPlacement.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class QuestPlacement : Quest
 {
     [SerializeField] Structure m_Structure;
+    [SerializeField] bool m_Removed = false;
 
     public override string GetTextual()
     {
-        return string.Format("Place {0}", m_Structure.name);
+        if (!m_Removed)
+        {
+            return string.Format("Place {0}", m_Structure.name);
+        }
+        else
+        {
+            return string.Format("Remove {0}", m_Structure.name);
+        }
     }
 
     public override bool IsComplete()
     {
-        return Manager.instance.GetStructureOfType(m_Structure) != null;
+        return (Manager.instance.GetStructureOfType(m_Structure) == null) == m_Removed;
     }
 }
