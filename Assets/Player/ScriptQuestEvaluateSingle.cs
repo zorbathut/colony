e78264b37657ede2d29196763a6585b548114558
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-27 04:00:51 -0800

    Add single-quest evaluation progression (needed for large forest)

---
+++ Assets/Player/ScriptQuestEvaluateSingle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptQuestEvaluateSingle : Script
+{
+    [SerializeField] Quest m_Quest;
+    public override bool Execute()
+    {
+        if (ScriptQuestEvaluate.ConsumeDebugOverride())
+        {
+            return true;
+        }
+
+        return m_Quest.IsComplete();
+    }
+}
