0b553f11f0a1cae74f91f48f531a7f94f4f61f5e
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-12-28 07:25:12 -0800

    Add wait script

---
+++ Assets/Player/ScriptWait.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptWait : Script
+{
+    [SerializeField] float m_Duration;
+    
+    public override bool Execute()
+    {
+        // this assumes we get called once per "update", whether that be Update or FixedUpdate, but right now we do, so, cool
+
+        m_Duration -= Time.deltaTime;
+
+        return m_Duration <= 0;
+    }
+}
